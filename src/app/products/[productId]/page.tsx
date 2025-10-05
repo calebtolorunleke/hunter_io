d46f66b3b09203a434c2b97c0b20d313eff14f16
Caleb Adebayo <calebtolorunleke@yahoo.com>
2025-10-04 21:20:04 -0700

    latest push on hunter_io

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,3 +1,22 @@
+import { Metadata } from "next";
+import { resolve } from "path";
+
+type Props = {
+  params: {
+    productId: string;
+  };
+};
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iphone ${params.productId}`);
+    }, 100);
+  });
+  return { title: `Product ${params.productId}` };
+};
+
 export default function productList({
   params,
 }: {
